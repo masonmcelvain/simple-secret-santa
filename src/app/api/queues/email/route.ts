80b654b8b55dea19130cd4aa8aad841d09268fa6
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-12-14 09:54:26 -0800

    feat: assign participants

--- src/app/api/queues/email/route.ts
+++ src/app/api/queues/email/route.ts
@@ -4,8 +4,27 @@ import { Queue } from "quirrel/next-app";
 export const EmailQueue = Queue<Participant[]>(
    "api/queues/email",
    async (participants) => {
-      console.log("Sending email to participants:", participants);
+      const assignments = assign(participants);
+      console.log("Sending email to participants:", assignments);
    },
 );
 
 export const POST = EmailQueue;
+
+const MAX_TRIES = 50;
+
+function assign(participants: Participant[]) {
+   for (let i = 0; i < MAX_TRIES; i++) {
+      const pairs = tryAssign(participants);
+      const isValid = pairs.every(([a, b]) => a.email !== b.email);
+      if (isValid) return pairs;
+   }
+   throw new Error("Failed to assign participants");
+}
+
+function tryAssign(participants: Participant[]): [Participant, Participant][] {
+   const shuffled = [...participants].sort(() => Math.random() - 0.5);
+   return participants.map((participant) => {
+      return [participant, shuffled.pop() as Participant];
+   });
+}
