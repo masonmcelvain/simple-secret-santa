0477d1adfb89205a6384be97aac002da99d8d32a
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-12-19 15:00:09 -0800

    fix(config): work around uglify-js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   webpack: (config, options) => {
+      // https://github.com/mjmlio/mjml/issues/1365#issuecomment-555835755
+      config.module.rules.push({
+         test: /node_modules\/uglify-js\/tools\/node\.js/,
+         loader: "null-loader",
+      });
+      return config;
+   },
+};
 
 module.exports = nextConfig;
