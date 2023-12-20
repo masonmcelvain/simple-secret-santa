78b09bf1f589a02e7e55091cc623b1a496216512
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-12-20 09:09:58 -0800

    feat: add badge section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { InputForm } from "@/components/InputForm";
+import { BadgeSection } from "@/sections/BadgeSection";
 
 export default function Page() {
    return (
-      <div>
+      <div className="flex flex-col space-y-4">
          <h1>Secret Santa</h1>
-         <p>Fast, easy, and safe, pick three!</p>
+         <p className="text-xl">Todo: add header with about page/popup</p>
+         <h2>How it works</h2>
+         <p className="text-xl">
+            You enter the names and emails of the folks you want to play Secret
+            Santa with. The app randomly assigns each participant to another and
+            emails each person with their assignment.
+         </p>
          <div className="flex w-full flex-col items-center">
             <InputForm />
          </div>
+         <BadgeSection />
       </div>
    );
 }
---
+++ src/sections/BadgeSection.tsx
@@ -0,0 +1,48 @@
+import {
+   BookOpenIcon,
+   CursorArrowRaysIcon,
+   ShieldCheckIcon,
+} from "@heroicons/react/24/outline";
+
+// TODO: add link to issues page
+export function BadgeSection() {
+   return (
+      <section className="flex flex-col items-center justify-between space-y-4 pt-4 md:flex-row md:space-x-8 md:space-y-0 md:px-24">
+         <div className="max-w-xs">
+            <div className="flex items-center justify-center space-x-2">
+               <CursorArrowRaysIcon className="h-8 w-8" />
+               <h3 className="m-0">No frills</h3>
+            </div>
+            <p className="text-center text-xl">
+               Intentionally simple so you can get back to the fun stuff.
+            </p>
+         </div>
+         <div className="max-w-xs">
+            <div className="flex items-center justify-center space-x-2">
+               <ShieldCheckIcon className="h-8 w-8" />
+               <h3 className="m-0">Safe</h3>
+            </div>
+            <p className="text-center text-xl">
+               Personal data is encrypted and never stored, shared, or sold.
+            </p>
+         </div>
+         <div className="max-w-xs">
+            <div className="flex items-center justify-center space-x-2">
+               <BookOpenIcon className="h-8 w-8" />
+               <h3 className="m-0">Open source</h3>
+            </div>
+            <p className="text-center text-xl">
+               Questions and requests are always welcome on{" "}
+               <a
+                  href="https://github.com/"
+                  target="_blank"
+                  className="rounded text-teal-500 focus:outline-none focus-visible:ring focus-visible:ring-chakra-focus"
+               >
+                  GitHub
+               </a>
+               .
+            </p>
+         </div>
+      </section>
+   );
+}
