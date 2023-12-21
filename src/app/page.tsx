52f5d306ee6f7759ec05f8d387b02c66e61ef68d
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-12-20 21:16:52 -0800

    refactor: add readme section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import { Form } from "./components/Form";
-import { BadgeSection } from "./sections/BadgeSection";
+import { BadgeSection, ReadmeSection } from "./sections";
 
 export default function Page() {
    return (
       <>
          <h1>Secret Santa</h1>
          <p className="text-xl">Todo: add header with about page/popup</p>
-         <section>
-            <h2>How it works</h2>
-            <ol className="list-none text-xl">
-               <li>
-                  ✍️ You enter the names and emails of the folks you want to
-                  play Secret Santa with.
-               </li>
-               <li>🧮 The app randomly assigns each participant to another.</li>
-               <li>📫️ Each person recieves an email with their assignment.</li>
-            </ol>
-         </section>
+         <ReadmeSection />
          <main className="flex w-full flex-col items-center">
             <Form />
          </main>
---
+++ src/app/sections/ReadmeSection.tsx
@@ -0,0 +1,15 @@
+export function ReadmeSection() {
+   return (
+      <section>
+         <h2>How it works</h2>
+         <ol className="list-none text-xl">
+            <li>
+               ✍️ You enter the names and emails of the folks you want to play
+               Secret Santa with.
+            </li>
+            <li>🧮 The app randomly assigns each participant to another.</li>
+            <li>📫️ Each person recieves an email with their assignment.</li>
+         </ol>
+      </section>
+   );
+}
