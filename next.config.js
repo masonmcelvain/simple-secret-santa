cc4fb3e612e74a9c2885d20b70506aac2b3d0a09
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2023-12-23 10:10:19 -0800

    fix(config): suppress console warning from mjml
    ```
    Critical dependency: the request of a dependency is an expression
    ```

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
          test: /node_modules\/uglify-js\/tools\/node\.js/,
          loader: "null-loader",
       });
+      // https://github.com/mjmlio/mjml/issues/438#issuecomment-352386596
+      config.module.exprContextCritical = false;
       return config;
    },
 };
