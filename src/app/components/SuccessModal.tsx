a56a86d2a3b33f7443747b38e70174823bd5737b
Mason McElvain <52104630+masonmcelvain@users.noreply.github.com>
2025-02-16 08:00:13 -0700

    feat: ugprade tailwind to v4

--- src/app/components/SuccessModal.tsx
+++ src/app/components/SuccessModal.tsx
@@ -25,7 +25,7 @@ export function SuccessModal({ open, setOpen }: SuccessModalProps) {
                leaveFrom="opacity-100"
                leaveTo="opacity-0"
             >
-               <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+               <div className="bg-opacity-75 fixed inset-0 bg-gray-500 transition-opacity" />
             </Transition.Child>
 
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -40,18 +40,18 @@ export function SuccessModal({ open, setOpen }: SuccessModalProps) {
                      leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                   >
                      <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                            <div className="sm:flex sm:items-start">
-                              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                              <div className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                                  <RocketLaunchIcon
                                     className="h-6 w-6 text-green-600"
                                     aria-hidden="true"
                                  />
                               </div>
-                              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                  <Dialog.Title
                                     as="h3"
-                                    className="text-base font-semibold leading-6 text-gray-900"
+                                    className="text-base leading-6 font-semibold text-gray-900"
                                  >
                                     Success!
                                  </Dialog.Title>
@@ -67,7 +67,7 @@ export function SuccessModal({ open, setOpen }: SuccessModalProps) {
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                            <button
                               type="button"
-                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
                               onClick={() => setOpen(false)}
                               ref={closeButtonRef}
                            >
